946861935aa4125df0166a472b8fe0c274f4f285
bak3839 <bak3839@naver.com>
2024-07-25 02:33:55 +0900

    Feat: 사용자의 반려견 목록 반환 기능 구현

--- src/main/java/com/kitcat/likelion/repository/PetRepository.java
+++ src/main/java/com/kitcat/likelion/repository/PetRepository.java
@@ -2,6 +2,10 @@ package com.kitcat.likelion.repository;
 
 import com.kitcat.likelion.domain.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    List<Pet> findPetsByUserId(@Param("userId") Long userId);
 }
--- src/main/java/com/kitcat/likelion/responseDTO/PetInfoDTO.java
+++ src/main/java/com/kitcat/likelion/responseDTO/PetInfoDTO.java
@@ -3,6 +3,7 @@ package com.kitcat.likelion.responseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class PetInfoDTO {
 
