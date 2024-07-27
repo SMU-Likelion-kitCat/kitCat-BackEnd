20ee42b214102dea9b3acb7b7005861bc5dd970e
bak3839 <bak3839@gmail.com>
2024-07-27 17:21:22 +0900

    Feat: Routine 열거형 수신 방법 수정

--- src/main/java/com/kitcat/likelion/domain/enumration/RoutineType.java
+++ src/main/java/com/kitcat/likelion/domain/enumration/RoutineType.java
@@ -1,9 +1,9 @@
 package com.kitcat.likelion.domain.enumration;
 
 public enum RoutineType {
-    TIME("시간 목표를 달성하는 방식"),
-    CALORIE("칼로리 목표를 달성하는 방식"),
-    DISTANCE("산책 거리 목표를 달성하는 방식");
+    TIME("시간 목표"),
+    CALORIE("칼로리 목표"),
+    DISTANCE("산책 거리 목표");
 
     private String routineType;
 
