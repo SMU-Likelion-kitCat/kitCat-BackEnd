13150bb1d50213f0e7b121b045149d7ea4bfc6d6
bak3839 <bak3839@naver.com>
2024-08-02 15:59:42 +0900

    Feat: postController의 postId를 pathvariable로 변경

--- src/main/java/com/kitcat/likelion/controller/PostController.java
+++ src/main/java/com/kitcat/likelion/controller/PostController.java
@@ -22,13 +22,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/api/v1/post")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/create")
     @Operation(summary = "게시글 작성 기능", description = "게시글작성에 사용되는 API")
@@ -46,15 +45,9 @@ public class PostController {
     }
     @Operation(summary = "댓글 작성 기능", description = "댓글 작성 사용되는 API")
     @ApiResponse(responseCode = "201", description = "댓글 작성 성공")
-    @Parameters({
-            @Parameter(name = "postId", description = "댓글이 달리는 게시글의 ID"),
-            @Parameter(name = "parentId", description = "대댓글일 경우 대댓글이 달리는 댓글의 ID(없으면 null로 작성)"),
-            @Parameter(name = "content", description = "댓글 내용"),
-
-    })
     @PostMapping("/comment")
     public String comment(@RequestBody PostCommentRequestDTO requestDTO,
-                                @AuthenticationPrincipal CustomUserDetails userDetails){
+                          @AuthenticationPrincipal CustomUserDetails userDetails){
 
         postService.createComment(userDetails.getUserId(), requestDTO);
 //        postService.createComment(1L, requestDTO);
@@ -64,19 +57,11 @@ public class PostController {
     @Operation(summary = "게시글을 자세히 보는 기능", description = "게시글을 들어갈 때 작성 사용되는 API")
     @ApiResponse(responseCode = "200", description = "게시글 보기")
     @Parameters({
-            @Parameter(name = "title", description = "게시글 이름"),
-            @Parameter(name = "content", description = "게시글 내용"),
-            @Parameter(name = "writer", description = "작성자"),
-            @Parameter(name = "commentCount", description = "댓글 갯수"),
-            @Parameter(name = "likeCount", description = "좋아요 갯수"),
-            @Parameter(name = "heartState", description = "내가 좋아요를 눌렀나 안눌렀나 상태"),
-            @Parameter(name = "createTime", description = "작성일"),
-            @Parameter(name = "comments", description = "댓글들"),
-            @Parameter(name = "photoName", description = "사진"),
+            @Parameter(name = "postId", description = "상세 조회 게시글 ID", example = "1")
     })
-    @GetMapping("/show")
-    public PostDetailDTO show(@RequestParam Long postId,
-                                 @AuthenticationPrincipal CustomUserDetails userDetails){
+    @GetMapping("/show/{postId}")
+    public PostDetailDTO show(@PathVariable("postId") Long postId,
+                              @AuthenticationPrincipal CustomUserDetails userDetails){
         return postService.findDetailPost(userDetails.getUserId(), postId);
     }
 
@@ -86,10 +71,10 @@ public class PostController {
             @ApiResponse(responseCode = "400", description = "중복좋아요 입력 실패")
     })
     @Parameters({
-            @Parameter(name = "postId", description = "좋아요를 누르는 게시글 ID")
+            @Parameter(name = "postId", description = "좋아요를 누르는 게시글 ID", example = "1")
     })
-    @GetMapping("/heart/insert")
-    public ResponseEntity<String> insertHeart(@RequestParam Long postId,
+    @GetMapping("/heart/insert/{postId}")
+    public ResponseEntity<String> insertHeart(@PathVariable("postId") Long postId,
                                       @AuthenticationPrincipal CustomUserDetails userDetails) {
 
         String heartStatus =  postService.insertHeart(userDetails.getUserId(), postId);
@@ -105,14 +90,12 @@ public class PostController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "좋아요 삭제 성공"),
             @ApiResponse(responseCode = "400", description = "존재하지 않는 좋아요 삭제")
-
-
     })
     @Parameters({
-            @Parameter(name = "postId", description = "삭제를 원하는 게시글 ID")
+            @Parameter(name = "postId", description = "삭제를 원하는 게시글 ID", example = "1")
     })
-    @GetMapping("/heart/delecte")
-    public ResponseEntity<String> deleteHeart(@RequestParam Long postId,
+    @GetMapping("/heart/delecte/{postId}")
+    public ResponseEntity<String> deleteHeart(@PathVariable("postId") Long postId,
                                 @AuthenticationPrincipal CustomUserDetails userDetails) {
 
         String heartStatus = postService.deleteHeart(userDetails.getUserId(), postId);
@@ -126,16 +109,6 @@ public class PostController {
     @Operation(summary = "게시글 목록보기(전체)", description = "게시글 전체 목록 볼 때 사용되는 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "전체 목록 조회 성공"),
-
-
-    })
-    @Parameters({
-            @Parameter(name = "content", description = "게시글 내용"),
-            @Parameter(name = "writer", description = "작성자"),
-            @Parameter(name = "commentCount", description = "댓글 수"),
-            @Parameter(name = "likeCount", description = "좋아요 수"),
-            @Parameter(name = "createTime", description = "작성일")
-
     })
     @GetMapping("/show/all")
     public List<PostListDTO> showAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
